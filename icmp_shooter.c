bbb7477a496ba1a8d3fc1c1bc640342d7fb30e06
Dominic Spadacene <accounts.github@dominic.spadacene.com>
2017-06-06 08:52:09 -0400

    can now send entire files!

--- icmp_shooter.c
+++ icmp_shooter.c
@@ -1,3 +1,4 @@
+#include<stdlib.h>
 #include<errno.h>
 #include <sys/cdefs.h>
 #include <sys/types.h>
@@ -8,8 +9,11 @@
 #include <arpa/inet.h>
 #include<string.h>
 #include<stdio.h>
+#include<sys/stat.h>
+#include<fcntl.h>
+#include<unistd.h>
 
-#define MAX_PEANTBTR_LEN 100
+#define MAX_PEANTBTR_LEN 512
 
 struct ouricmphdr
 {
@@ -34,47 +38,78 @@ struct ouricmphdr
 };
 
 
-uint16_t ip_checksum(void* vdata,size_t length);
+#define MAX_MSG_SIZE (MAX_PEANTBTR_LEN - sizeof(struct ouricmphdr))
+
 
 struct peanutbutter {
     struct ouricmphdr mycmp;
-    char secret_package[MAX_PEANTBTR_LEN - sizeof(struct ouricmphdr)];
-
+    char secret_package[MAX_MSG_SIZE];
 };
 
+/*int prep_pkt(struct peanutbutter * pb, void *buf, ssize_t count);*/
+uint16_t ip_checksum(void* vdata,size_t length);
 
 int main(int argc, char ** argv){
 
-    char * destip = "11.0.0.2";
-    char * srcip = "11.0.0.3";
-
     struct sockaddr_in ip4addr;
-    struct peanutbutter mypb;
+    char * destip = "11.0.0.1";
+    char * srcip = "11.0.0.2";
     ip4addr.sin_family = AF_INET;
     int res = inet_aton(destip,&(ip4addr.sin_addr));
-    printf("success? %d\n", res);
-
-    mypb.mycmp.type = ICMP_INFO_REPLY;
-    mypb.mycmp.code = 0;  //  ¯\_(ツ)_/¯
-    mypb.mycmp.checksum=0;
-    mypb.mycmp.peanut=2;
-    mypb.mycmp.un.echo.id = 0xbeef;
-    mypb.mycmp.un.echo.sequence = 512;
-    mypb.mycmp.un.gateway = 0xfeedface;
-    mypb.mycmp.un.frag.mtu = 0x0011;
-
-    strcpy(mypb.secret_package, "tickle me TICKLE TICKLE TICKLE TICKLE TICKLE TICKLE TICKLE TICKLE TICKLE");
-
-	mypb.mycmp.checksum=ip_checksum(&mypb,sizeof(struct peanutbutter));
     int socky = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
     if(socky == -1)
         printf("socky error is: %s\n", strerror(errno));
-    int ret = sendto(socky,&mypb, sizeof(mypb), 0, (struct sockaddr *) &ip4addr, sizeof(struct sockaddr));
-    if(ret == -1)
-        printf("sendto error is: %s\n", strerror(errno));
-    printf("BAMMMMMM\n");
+
+    struct peanutbutter pb;
+    /*pb = (struct peanutbutter*)malloc(sizeof(struct peanutbutter));*/
+    pb.mycmp.type = ICMP_INFO_REPLY;
+    pb.mycmp.code = 0;  //  ¯\_(ツ)_/¯
+    pb.mycmp.checksum=0;
+    pb.mycmp.peanut=2;
+    pb.mycmp.un.echo.id = 0xbeef;
+    pb.mycmp.un.echo.sequence = 512;
+    pb.mycmp.un.gateway = 0xfeedface;
+    pb.mycmp.un.frag.mtu = 0x0011;
+    
+    // argv == icmp_shooter fiel_name
+    char * filename = argv[1];
+    
+    // open file 
+    int fd = open(filename, O_RDONLY);
+
+    // loop contents until less PACKETSIZE and shoot
+    ssize_t bytes_read;
+    char buf[MAX_MSG_SIZE];
+    while((bytes_read = read(fd, buf, MAX_MSG_SIZE)) != 0){
+        /*res = prep_pkt(pb, buf, bytes_read);*/
+
+        strncpy(pb.secret_package, buf, bytes_read);
+        pb.mycmp.checksum=ip_checksum(&pb, sizeof(struct peanutbutter));
+        int ret = sendto(socky, &pb, sizeof(pb), 0, (struct sockaddr *) &ip4addr, sizeof(struct sockaddr));
+        if(ret == -1)
+            printf("sendto error is: %s\n", strerror(errno));
+        printf("BAMMMMMM\n");
+        /*free(pb);*/
+        /*pb = NULL;*/
+    } 
+    
     return 0;
 }
+
+/*int prep_pkt(struct peanutbutter * pb, void *buf, ssize_t count){*/
+    /*pb = (struct peanutbutter*)malloc(sizeof(struct peanutbutter));*/
+    /*pb->mycmp.type = ICMP_INFO_REPLY;*/
+    /*pb->mycmp.code = 0;  //  ¯\_(ツ)_/¯*/
+    /*pb->mycmp.checksum=0;*/
+    /*pb->mycmp.peanut=2;*/
+    /*pb->mycmp.un.echo.id = 0xbeef;*/
+    /*pb->mycmp.un.echo.sequence = 512;*/
+    /*pb->mycmp.un.gateway = 0xfeedface;*/
+    /*pb->mycmp.un.frag.mtu = 0x0011;*/
+    /*strncpy(pb->secret_package, buf, count);*/
+    /*pb->mycmp.checksum=ip_checksum(pb, sizeof(struct peanutbutter));*/
+    /*return 0;*/
+/*}*/
 
 uint16_t ip_checksum(void* vdata,size_t length) {
     // Cast the data pointer to one that can be indexed.
